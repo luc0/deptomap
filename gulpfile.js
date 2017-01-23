218a17330790f025d5b3a6a9d1552b4f0dafefd6
luciano perez <luciano.perez@aerolab.com.ar>
2017-01-23 16:25:17 -0300

    Babel using Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,15 @@ var gulp = require('gulp'),
   nodemon = require('gulp-nodemon'),
   plumber = require('gulp-plumber'),
   livereload = require('gulp-livereload'),
-  sass = require('gulp-sass');
+  sass = require('gulp-sass'),
+  babel = require('gulp-babel'),
+  del = require('del'),
+  exec = require('child_process').exec;
+
+const paths = {
+  allSrcJs: 'src/**/*.js',
+  libDir: 'lib',
+};
 
 gulp.task('sass', function () {
   gulp.src('./public/css/*.scss')
@@ -14,7 +22,9 @@ gulp.task('sass', function () {
 
 gulp.task('watch', function() {
   gulp.watch('./public/css/*.scss', ['sass']);
+  gulp.watch(paths.allSrcJs, ['main']);
 });
+
 
 gulp.task('develop', function () {
   livereload.listen();
@@ -33,7 +43,33 @@ gulp.task('develop', function () {
   });
 });
 
+/* BABEL */
+
+gulp.task('clean', () => {
+  return del(paths.libDir);
+});
+
+gulp.task('build', ['clean'], () => {
+  return gulp.src(paths.allSrcJs)
+    .pipe(babel())
+    .pipe(gulp.dest(paths.libDir));
+});
+
+gulp.task('babel', ['build'], (callback) => {
+  exec(`node ${paths.libDir}`, (error, stdout) => {
+    console.log(stdout);
+    return callback(error);
+  });
+});
+
+/* --- */
+
+gulp.task('watch', () => {
+  gulp.watch(paths.allSrcJs, ['babel']);
+});
+
 gulp.task('default', [
+  'babel',
   'sass',
   'develop',
   'watch'
